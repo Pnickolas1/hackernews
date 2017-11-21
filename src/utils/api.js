8c1a894cf711a3946468733123d2f923369f44ca
Peter Tountas <petertountas1@gmail.com>
2017-11-20 19:39:19 -0600

    api complete and

--- src/utils/api.js
+++ src/utils/api.js
@@ -14,3 +14,61 @@ export function getAllCategories(){
 export function getAllPosts(){
   return axios.get(API_URL+"/posts", APP_HEADER)
 }
+
+export function createPost(post){
+  return axios.put(API_URL+"/posts", post,APP_HEADER)
+}
+
+export function updatePost(post){
+  return axios.put(API_URL+"/posts/"+ post.id, post, APP_HEADER)
+}
+
+export function getPostsByCategory(category){
+  return axios.get(API_URL+"/"+ category + "/posts", APP_HEADER)
+}
+
+export function getPostById(postId){
+  return axios.get(API_URL+"/posts/"+ postId + APP_HEADER)
+}
+
+export function incrementPostVote(postId){
+  return axios.put(API_URL + "/posts/" + postId, {option: "upVote"}, APP_HEADER)
+}
+
+export function decrementPostVote(postId){
+  return axios.put(API_URL + "/posts/" + postId, {optionL: "downVote"}, APP_HEADER)
+}
+
+export function deletePost(postId){
+  return axios.put(API_URL + "/posts/" + postId, APP_HEADER)
+}
+
+// COMMENTS ENDPOINTS
+
+export function getComments(){
+  return axios.get(API_URL + "/comments/", APP_HEADER)
+}
+
+export function getCommentsById(postId){
+  return axios.get(API_URL + "/posts/"+ postId +'/comments', APP_HEADER )
+}
+
+export function createComment(comment){
+  return axios.get(API_URL + '/comments', comment,APP_HEADER)
+}
+
+export function updateComment(comment){
+  return axios.put(API_URL + '/comments/' + comment.id, comment, APP_HEADER)
+}
+
+export function deleteComment(commentId){
+  return axios.delete(API_URL + "/comments/" + commentId, APP_HEADER)
+}
+
+export function incrementCommentVote(commentId){
+  return axios.post(API_URL + '/comments/' + commentId, {option: "upVote"}, APP_HEADER)
+}
+
+export function decrementCommentVote(commentId){
+  return axios.post(API_URL + "/comments/" + commentId, {option : 'downVote'}, APP_HEADER)
+}
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,4 +2,20 @@ import _ from 'lodash';
 import moment from 'moment';
 import shortid from 'shortid'
 
+export function sort(state, type="voteScore"){
+  return _.orderBy(state, type, 'asc')
+}
 
+export function time(timestamp){
+  return moment(timestamp).fromNow()
+}
+
+export const generateId = shortid.generate()
+
+export function isEmpty(object){
+  return _.isEmpty(object)
+}
+
+export function getCommentsCount(comments, postId){
+  return comments.filter(comment => comment.parentId == postId).length
+}
