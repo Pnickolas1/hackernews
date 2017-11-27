f6cee7a9e655e432a882606966d1b9e12830428d
Peter Tountas <petertountas1@gmail.com>
2017-11-26 20:27:37 -0600

    making the vote file

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
+import Comments from '../comments';
+import * as postActions from '../../actions/posts'
+import * as commentActions from '../../actions/comments'
+import * as helpers from '../../utils/helpers';
+import * as categoryActions from '../../actions/categories';
+import Vote from '../Vote';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux'
+import Modal from 'react-modal';
+import { Redirect } from 'react-router';
 
 
-class Post extends Component {
+class Post extends Component{
+
+
   render(){
-    return (
+    return(
       <div>
-        <p> this is a place holder </p>
+        <p>test this</p>
       </div>
     )
   }
 }
-export default Post
+
+
+
+export default Post;
---
+++ src/components/Vote.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import {bindActionCreators} from 'redux'
+import * as postActions from '../actions/posts'
+import * as commentActions from '../actions/comments'
+import {connect} from 'react-redux'
+
+
+class Vote extends Component {
+  upVote = e => {
+    e.preventDefault()
+    switch(this.props.type){
+      case "post":
+        return this.props.actions.addVote(this.props.id)
+      case "comment":
+        return this.props.actions.addCommentVote(this.props.id)
+      default:
+        console.log("Error here, check those code base")
+    }
+  }
+
+
+  downVote = e => {
+    e.preventDefault()
+    switch(this.props.type){
+    case "post":
+      return this.props.actions.subtractVote(this.props.id)
+    case "comment":
+      return this.props.actions.subtractCommentVote(this.props.id)
+    default:
+      console.log("Error here, check the code base")
+    }
+  }
+
+  compnonentWillRecieveProps(nextProps){
+    this.setState({
+      size: nextProps.size,
+      id: nextProps.id,
+      type: nextProps.type
+    })
+  }
+
+
+  render(){
+    return(
+      <div className="row">
+        <div className="col-md-12">
+          <button className="btn btn-success btn-sm" style={{height: this.props.size, width: this.props.size, lineHeight: 0}} onClick={this.upVote}>
+            <i className="fa fa-plus" style={{fontSize: 10, marginLeft: -3, position: "absolute", marginTop: -5}}></i>
+          </button>
+          <button className="btn btn-danger btn-sm" style={{height: this.props.size, width: this.props.size, lineHeight: 0, marginLeft: 10}} onClick={this.downVote}>
+            <i className="fa fa-minus"  style={{fontSize: 10, marginLeft: -3, position: "absolute", marginTop: -5}}></i>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
