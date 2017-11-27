6377faacd1d81982962ae826f736f2eff0b38dcc
Peter Tountas <petertountas1@gmail.com>
2017-11-27 12:30:53 -0600

    ui updates

--- src/route.js
+++ src/route.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CategoryPage from './components/Category/CategoryPage';
 import PostsPage from './components/Posts/PostsPage'
+import PostPage from './components/Posts/PostPage'
 import { Route, Switch } from 'react-router';
 
 
@@ -8,8 +9,8 @@ export default (
   <div>
     <Switch>
       <Route exact path="/" component={PostsPage} />
-      <Route expath path="/postspage" component={PostsPage} />
-      <Route exact path="/:category" component={CategoryPage} />
+      <Route exact path=":category" component={CategoryPage} />
+      <Route expath path="/:category/:id" component={PostPage} />
     </Switch>
   </div>
 );
